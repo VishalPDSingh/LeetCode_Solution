4418cc8b3e1b9609c0ab431e29d97d5330e77d6b
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2025-07-25 05:13:53 +0530

    Time: 1 ms (99.54%) | Memory: 57.2 MB (42.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,31 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        
-       int maxSum = Integer.MIN_VALUE;
-        int currSum = 0;
+         int currentSum = nums[0];
+        int maxSum = nums[0];
 
-        // Check if all numbers are negative
-        boolean allNegative = true;
-        int maxElement = nums[0];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                allNegative = false;
-            }
-            maxElement = Math.max(maxElement, nums[i]);
-        }
-
-        // If all elements are negative, return the maximum element
-        if (allNegative) {
-            return maxElement;
-        }
-
-        // Apply Kadane's Algorithm
-        for (int i = 0; i < nums.length; i++) {
-            currSum += nums[i];
-            if (currSum < 0) {
-                currSum = 0;
-            }
-            maxSum = Math.max(maxSum, currSum);
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
         return maxSum;
