909fc8382afe8015c5096fb8144ebe26263b49cd
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2025-07-26 05:11:56 +0530

    Time: 1 ms (9.17%) | Memory: 43.1 MB (32.48%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        
+        int maxMoney = 0;
+
+        for(int i=0; i<accounts.length; i++)
+        {
+            int sum = 0; // Reset sum for each customer
+            for(int j=0; j<accounts[0].length; j++)
+            {
+                sum = sum+accounts[i][j];
+                if(maxMoney<sum)
+                {
+                    maxMoney = sum;
+                }
+            }
+        }
+        return maxMoney;
+    }
+}
