de097b2d3b97ba755039d96647a535779a59e7f4
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2025-08-30 23:48:42 +0530

    Time: 1 ms (63.34%) | Memory: 41.6 MB (61.89%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,20 +1,6 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < command.length(); ) {
-            if (command.charAt(i) == 'G') {
-                sb.append("G");
-                i++;
-            } else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
-                sb.append("o");
-                i += 2;
-            } else { // must be "(al)"
-                sb.append("al");
-                i += 4;
-            }
-        }
-
-        return sb.toString();
+        
+         return command.replace("()", "o").replace("(al)", "al");
     }
 }
